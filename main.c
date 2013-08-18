2132f838166c9fd8eaefabbfdaf86d035d57286c
Mike Gaffney <mike@uberu.com>
2013-08-18 13:13:18 -0700

    Blinking One Light

--- main.c
+++ main.c
@@ -2,4 +2,23 @@
 
 void main()
 {
+    // Declare a on/off for the light
+
+    unsigned char fOn = 0;
+
+    // Set pin 0 on Port A to output (0) and the rest to not output
+
+    TRISA = 0b11111110;
+    // TRISAbits.TRISA0 = 0;
+
+    // Forever
+
+    while (1)
+    {
+        // Flop our toggle
+
+        fOn = !fOn;
+
+        PORTA = fOn;
+    }
 }
